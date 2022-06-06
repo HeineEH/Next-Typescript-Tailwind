2447dfcb6f7e5cdd4db892582f472d4364f1244f
HeineEH <38105981+HeineEH@users.noreply.github.com>
2022-06-06 14:01:23 +0200

    Next-Typescript-Tailwind starter v1

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+const IndexPage = () => (
+  <div className="flex justify-center items-center h-full w-full bg-gradient-to-t from-pink-500 to-yellow-500">
+    <main className="w-1/2 h-2/3">
+      <h1 className="text-2xl text-center">
+        Next.js + Typescript + Tailwind CSS
+      </h1>
+      <p className="text-center text-blue-400 underline font-bold">
+        <a
+          target="_blank"
+          href="https://github.com/HeineEH/Next-Typescript-Tailwind"
+        >
+          Github Repo
+        </a>
+      </p>
+    </main>
+  </div>
+);
+
+export default IndexPage;
